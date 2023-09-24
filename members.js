955db0bd4065011e4af58150abbdadf646156395
Adam Stratilík <adam.stratilik@student.ssnd.sk>
2023-09-24 20:31:14 +0000

    Copilot second commit

---
+++ members.js
@@ -0,0 +1,5 @@
+function skillsMember()) {
+    var skills = ['HTML', 'CSS', 'JS', 'React', 'Node']
+    var skillsMember = skills[Math.floor(Math.random() * skills.length)]
+    return skillsMember
+}
